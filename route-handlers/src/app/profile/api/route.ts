c0df80d96239b0fc98e0d154a447f71c722ef0cd
krunal Mali <kkmali@enacton.com>
2025-04-09 19:54:31 +0530

    routes handler

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,19 @@
-export async function GET() {
-    return new Response("Profile API data");
+import { NextRequest } from "next/server";
+import { headers , cookies } from "next/headers";
+
+export async function GET(request:NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
+
+    console.log(requestHeaders.get("Authorization"))
+    // console.log(headerList.get("Authorization"))
+    // console.log(cookies().get("theme"))
+
+    return new Response("<h1>Profile API data</h1>",{
+        headers:{
+            "content-type":"text/html",
+            "set-cookie":"theme=dark",
+        },
+    }
+    );
 }
